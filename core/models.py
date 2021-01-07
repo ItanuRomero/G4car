df9153d408dd77a7280d43f9621d3fd4414c1527
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2021-01-07 20:56:24 -0300

    deploy

---
+++ core/models.py
@@ -0,0 +1,95 @@
+from math import ceil, floor, trunc
+
+from django.db import models
+
+# Create your models here.
+
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=100, blank=True, null=True)
+    complemento = models.CharField(max_length=50, blank=True, null=True)
+    bairro = models.CharField(max_length=100, blank=True, null=True)
+    cidade = models.CharField(max_length=100, blank=True, null=True)
+    cep = models.CharField(max_length=10, blank=True, null=True)
+    email = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=30)
+    foto = models.ImageField(upload_to='fotos_clientes', blank=True, null=True)
+
+    def __str__(self):
+        return self.nome + f' ( {str(self.id)} )'
+
+    class Meta:
+        verbose_name_plural = 'Clientes'
+
+
+class Veiculo(models.Model):
+    fabricante = models.CharField(max_length=50)
+    modelo = models.CharField(max_length=50)
+    ano = models.CharField(max_length=4, blank=True, null=True)
+    cor = models.CharField(max_length=20, blank=True, null=True)
+    placa = models.CharField(max_length=10)
+    foto = models.ImageField(upload_to='fotos_veiculos', blank=True, null=True)
+    id_cliente = models.ForeignKey("Cliente", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.placa} - ( cliente: {self.id_cliente} )'
+
+    class Meta:
+        verbose_name_plural = 'Veículos'
+
+
+class Parametro(models.Model):
+    descricao = models.CharField(max_length=20)
+    valor = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.descricao} - R${self.valor}'
+
+    class Meta:
+        verbose_name_plural = 'Parâmetros'
+
+
+class Movimento(models.Model):
+    data_entrada = models.DateTimeField(auto_now_add=None)
+    data_saida = models.DateTimeField(auto_now_add=None, blank=True, null=True)
+    id_veiculo = models.ForeignKey("Veiculo", on_delete=models.CASCADE)
+    valor_hora = models.ForeignKey("Parametro", on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+
+    def __str__(self):
+        return f'({self.id}) - {self.data_entrada} - {self.id_veiculo.placa}' \
+               f' - - {self.id_veiculo.modelo} de cor {self.id_veiculo.cor}'
+
+    class Meta:
+        verbose_name_plural = 'Movimentos'
+
+    # regra de negocio para calcular o total baseado no checkout
+    def calcula_total(self):
+        if self.data_saida:
+            if self.data_saida < self.data_entrada:
+                return 'erro'
+            horas = (self.data_saida - self.data_entrada).total_seconds() / 3600
+            minutos = (horas * 60) - (trunc(horas) * 60)
+            if minutos > 15:
+                horas = ceil(horas)
+            else:
+                horas = floor(horas)
+            # obj = Parametro.objects.get(id=self.valor_hora) // caso for um parametro opcional
+            self.total = horas * self.valor_hora.valor # ai substitui por obj.valor
+            return self.total
+        return 0.0
+
+
+class Mensalista(models.Model):
+    mensalidade = models.ForeignKey("Parametro", on_delete=models.CASCADE)
+    id_veiculo = models.ForeignKey("Veiculo", on_delete=models.CASCADE)
+    observacao = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.id_veiculo.placa} - ' \
+               f'{self.id_veiculo.modelo} - - ' \
+               f'Mensalidade - R${self.mensalidade.valor}'
+
+    class Meta:
+        verbose_name_plural = 'Mensalistas'
---
+++ core/views.py
@@ -0,0 +1,221 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from core.models import Cliente, Veiculo, Parametro, \
+                        Movimento, Mensalista
+from core.forms import FormCliente, FormVeiculo, \
+    FormParametro, FormMovimento, FormMensalista
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'core/index.html')
+
+
+class Registrar(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'
+
+
+@login_required
+def cadastro_cliente(request):
+    form = FormCliente(request.POST or None, request.FILES or None)
+    contexto = {'form': form, 'acao': 'Cadastro do cliente', 'titulo': 'Cadastrar cliente'}
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem_clientes')
+    else:
+        return render(request, 'core/cadastro_cliente.html', contexto)
+
+
+@login_required
+def listagem_clientes(request):
+    if request.POST:
+        if request.POST['nome']:
+            clientes = Cliente.objects.filter(nome=request.POST['nome'])
+        else:
+            clientes = Cliente.objects.all()
+    else:
+        clientes = Cliente.objects.all()
+    contexto = {'clientes': clientes}
+    return render(request, 'core/listagem_clientes.html', contexto)
+
+
+@login_required
+def cadastro_veiculo(request):
+    form = FormVeiculo(request.POST or None, request.FILES or None)
+    contexto = {'form': form, 'acao': 'Atualizar o cadastro de veiculo', 'titulo': 'Atualizar cadastro'}
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem_veiculos')
+    else:
+        return render(request, 'core/cadastro_veiculo.html', contexto)
+
+
+@login_required
+def listagem_veiculos(request):
+    veiculos = Veiculo.objects.all()
+    contexto = {'veiculos': veiculos}
+    return render(request, 'core/listagem_veiculos.html', contexto)
+
+
+@login_required
+def atualiza_cliente(request, id):
+    cliente_selecionado = Cliente.objects.get(id=id)
+    form = FormCliente(request.POST or None, request.FILES or None, instance=cliente_selecionado)
+    contexto = {'form': form, 'acao': 'Atualizar o cadastro do cliente', 'titulo': 'Atualizar cadastro'}
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem_clientes')
+    else:
+        return render(request, 'core/cadastro_cliente.html', contexto)
+
+
+@login_required
+def atualiza_veiculo(request, id):
+    veiculo_selecionado = Veiculo.objects.get(id=id)
+    form = FormVeiculo(request.POST or None, request.FILES or None, instance=veiculo_selecionado)
+    contexto = {'form': form, 'acao': 'Atualizar o cadastro de veiculo', 'titulo': 'Atualizar cadastro'}
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem_veiculos')
+    else:
+        return render(request, 'core/cadastro_veiculo.html', contexto)
+
+
+@login_required
+def exclui_cliente(request, id):
+    try:
+        cliente_selecionado = Cliente.objects.get(id=id)
+        contexto = {'acao': cliente_selecionado.nome, 'redirect': '/listagem_clientes/'}
+        if request.POST:
+            cliente_selecionado.delete()
+            return redirect('url_listagem_clientes')
+        else:
+            return render(request, 'core/confirma_exclusao.html', contexto)
+    except:
+        redirect('url_listagem_clientes')
+
+
+@login_required
+def exclui_veiculo(request, id):
+    try:
+        veiculo_selecionado = Veiculo.objects.get(id=id)
+        if request.POST:
+            veiculo_selecionado.delete()
+            return redirect('url_listagem_veiculos')
+        else:
+            return render(request, 'core/confirma_exclusao.html', {'acao': veiculo_selecionado.modelo, 'redirect': '/listagem_veiculos/'})
+    except:
+        return redirect('url_listagem_veiculos')
+
+
+@login_required
+def cadastro_parametro(request):
+    form = FormParametro(request.POST or None)
+    contexto = {'form': form, 'acao': 'Cadastro de Parametro', 'titulo': 'Cadastro de Parametro'}
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem_parametros')
+    else:
+        return render(request, 'core/cadastro_parametro.html', contexto)
+
+
+@login_required
+def listagem_parametros(request):
+    dados = Parametro.objects.all()
+    contexto = {'parametros': dados}
+    return render(request, 'core/listagem_parametros.html', contexto)
+
+
+@login_required
+def cadastro_mensalista(request):
+    form = FormMensalista(request.POST or None)
+    contexto = {'form': form, 'acao': 'Cadastro de mensalista', 'titulo': 'Cadastro de mensalista'}
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem_mensalistas')
+    else:
+        return render(request, 'core/cadastro_mensalista.html', contexto)
+
+
+@login_required
+def listagem_mensalistas(request):
+    dados = Mensalista.objects.all()
+    contexto = {'mensalistas': dados}
+    return render(request, 'core/listagem_mensalistas.html', contexto)
+
+
+@login_required
+def atualiza_parametro(request, id):
+    try:
+        obj = Parametro.objects.get(id=id)
+        form = FormParametro(request.POST or None, instance=obj)
+        contexto = {'form': form, 'acao': 'Atualizacao de parametro'}
+        if form.is_valid():
+            form.save()
+            return redirect('url_listagem_parametros')
+        else:
+            return render(request, 'core/cadastro_parametro.html', contexto)
+    except:
+        return redirect('url_listagem_parametros')
+
+
+@login_required
+def exclui_parametro(request, id):
+    obj = Parametro.objects.get(id=id)
+    contexto = {'acao': obj.descricao, 'redirect': '/listagem_parametros/'}
+    if request.method == 'POST':
+        obj.delete()
+        return redirect('url_listagem_parametros')
+    else:
+        return render(request, 'core/confirma_exclusao.html', contexto)
+
+
+@login_required
+def cadastro_movimento(request):
+    if request.user.is_staff:
+        form = FormMovimento(request.POST or None)
+        contexto = {'form': form, 'titulo': 'Cadastro de movimento', 'acao': 'Cadastro de Movimento'}
+        if form.is_valid():
+            form.save()
+            return redirect('url_listagem_movimentos')
+        return render(request, 'core/cadastro_movimento.html', contexto)
+    else:
+        contexto = {'erro': 'Você não tem permissão para executar este procedimento, '
+                            'procure o seu gerente.'}
+        return render(request, 'core/erro.html', contexto)
+
+
+@login_required
+def listagem_movimentos(request):
+    dados = Movimento.objects.all()
+    contexto = {'movimentos': dados}
+    return render(request, 'core/listagem_movimentos.html', contexto)
+
+
+@login_required
+def atualiza_movimento(request, id):
+    if request.user.is_staff:
+        try:
+            obj = Movimento.objects.get(id=id)
+            form = FormMovimento(request.POST or None, instance=obj)
+            contexto = {'form': form, 'acao': 'Atualizacao de Movimento', 'titulo': 'Atualiza Movimento - G4car'}
+            if form.is_valid():
+                retorno = obj.calcula_total()
+                if retorno == 'erro':
+                    contexto = {'erro': 'Valor de data de saida menor que de entrada, favor realizar novamente'}
+                    return render(request, 'core/erro.html', contexto)
+                form.save()
+                return redirect('url_listagem_movimentos')
+            else:
+                return render(request, 'core/cadastro_movimento.html', contexto)
+        except:
+            return redirect('url_listagem_movimentos')
+    else:
+        contexto = {'erro': 'Você não tem permissão para executar este procedimento, '
+                            'procure o seu gerente.'}
+        return render(request, 'core/erro.html', contexto)
